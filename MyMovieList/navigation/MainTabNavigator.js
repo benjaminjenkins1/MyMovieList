96519c43b8dd054451ed292e613799609760e12b
Benjamin Jenkins <benjamin.jenkins1@marist.edu>
2019-01-21 15:52:56 -0500

    first commit

---
+++ MyMovieList/navigation/MainTabNavigator.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios' ? 'ios-home' : 'md-home'
+      }
+    />
+  ),
+};
+
+const ListsStack = createStackNavigator({
+  Lists: LinksScreen,
+});
+
+ListsStack.navigationOptions = {
+  tabBarLabel: 'Lists',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-list' : 'md-list'}
+    />
+  ),
+};
+
+const ProfileStack = createStackNavigator({
+  Profile: SettingsScreen,
+});
+
+ProfileStack.navigationOptions = {
+  tabBarLabel: 'Profile',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  ListsStack,
+  ProfileStack,
+});
